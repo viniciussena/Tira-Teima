9019690f8fe7a1ed57e5f0516ada2e65d13558ec
Vinícius <Vincius@vini-note.tjdf.gov.br>
2011-02-14 09:59:40 -0200

    - Exemplo do for.

---
+++ testes/FontesC/lacos.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ 
+int main(){
+    int max,cont;
+    scanf("%d",&max);
+    for(cont = 0; cont <= max; cont++){
+        printf("valor: %d\n",cont);              
+    }
+    return 0;
+}
